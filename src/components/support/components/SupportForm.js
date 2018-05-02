ea2d0adc28cfc1d3d7b506478dbdfba2877afd1b
Jon Ambas <jon@jonambas.com>
2018-05-02 14:27:58 -0400

    FE-328 Support issue type/subject

--- src/components/support/components/SupportForm.js
+++ src/components/support/components/SupportForm.js
@@ -1,17 +1,27 @@
+/* eslint-disable */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm, formValueSelector } from 'redux-form';
 import { Button, Panel } from '@sparkpost/matchbox';
-import { TextFieldWrapper } from 'src/components';
+import { TextFieldWrapper, SelectWrapper } from 'src/components';
 import { required, minLength, maxFileSize } from 'src/helpers/validation';
 import config from 'src/config';
 import styles from './SupportForm.module.scss';
 import FileFieldWrapper from 'src/components/reduxFormWrappers/FileFieldWrapper';
+import _ from 'lodash';
 
 const formName = 'supportForm';
+const types = [
+  { label: 'Technical errors', value: 'Errors', messageLabel: 'Tell us more about your issue' },
+  { label: 'Billing problems', value: 'Billing', messageLabel: 'Tell us more about your billing issue' },
+  { label: 'Canceling my account', value: 'Compliance-Cancel', messageLabel: 'Tell us why you are leaving' },
+  { label: 'Unsuspending my account', value: 'Compliance-Suspension', messageLabel: 'Why do you think your account should be unsuspended?' },
+  { label: 'Increasing my daily sending limits', value: 'Daily Limits', messageLabel: 'What limit do you need and why?' },
+  { label: 'Another issue', value: 'Support', messageLabel: 'Tell us more about your issue' }
+];
 
 export class SupportForm extends Component {
-  renderSuccess() {
+  renderSuccess () {
     const { ticketId, onContinue } = this.props;
 
     return <div className={styles.SupportForm}>
@@ -24,20 +34,24 @@ export class SupportForm extends Component {
     </div>;
   }
 
-  reset(parentReset) {
+  reset (parentReset) {
     this.props.reset(formName);
     return parentReset();
   }
 
-  renderForm() {
+  renderForm () {
     const {
       pristine,
       invalid,
       submitting,
       handleSubmit,
       onSubmit,
-      onCancel
+      onCancel,
+      subject
     } = this.props;
+
+    const selectedType = _.find(types, { value: subject });
+    const messageLabel = _.get(selectedType, 'messageLabel', '');
 
     return <div className={styles.SupportForm}>
       <Panel.Section>
@@ -47,20 +61,19 @@ export class SupportForm extends Component {
         <Panel.Section>
           <Field
             name='subject'
-            label='Subject'
-            placeholder='Give your issue a title'
+            label='I need help with...'
+            helpText='Additional support options are available on paid plans'
             inlineErrors={true}
             autoFocus={true}
             disabled={submitting}
-            validate={[required, minLength(5)]}
-            component={TextFieldWrapper} />
+            options={types}
+            component={SelectWrapper} />
           <Field
             multiline
             rows={10}
             resize='none'
             name='message'
-            label='Message'
-            placeholder='Give us details about your issue'
+            label={messageLabel}
             inlineErrors={true}
             disabled={submitting}
             validate={required}
@@ -85,7 +98,7 @@ export class SupportForm extends Component {
     </div>;
   }
 
-  render() {
+  render () {
     if (this.props.submitSucceeded) {
       return this.renderSuccess();
     }
@@ -93,8 +106,13 @@ export class SupportForm extends Component {
   }
 }
 
-const mapStateToProps = ({ support }) => ({
-  ticketId: support.ticketId
+const selector = formValueSelector(formName);
+const mapStateToProps = (state) => ({
+  ticketId: state.support.ticketId,
+  subject: selector(state, 'subject'),
+  initialValues: {
+    subject: 'Errors'
+  }
 });
 
 const ReduxSupportForm = reduxForm({ form: formName })(SupportForm);
