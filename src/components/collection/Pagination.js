9cd6438aa67491c333fc6818d51c658e51e2a30d
Kevin Chu <kevinchu1337@gmail.com>
2018-06-20 18:44:36 -0400

    coordinates add

--- src/components/collection/Pagination.js
+++ src/components/collection/Pagination.js
@@ -4,21 +4,46 @@ import classnames from 'classnames';
 import styles from './Pagination.module.scss';
 import Papa from 'papaparse';
 import _ from 'lodash';
+import csv from 'csvtojson';
 
 export const defaultPerPageButtons = [10, 25, 50, 100];
-
+const coordinates = [];
+export { getCoords };
+function getCoords() {
+  return coordinates;
+}
 class CollectionPagination extends Component {
   formatToCsv = () => {
     const { data: rows } = this.props;
     // we are doing this because certain keys are objects/array which papa parse doesn't stringify
     const mappedRows = _.map(rows, (row) => _.mapValues(row, (value) => _.isObject(value) || _.isArray(value) ? JSON.stringify(value) : value));
     const data = Papa.unparse(mappedRows);
+    csv({
+      noheader: false,
+      output: 'csv'
+    })
+      .fromString(data)
+      .then((csvRow) => {
+        csvRow.forEach(function (row) {
+          try {
+            const location = JSON.parse(row[5]);
+            coordinates.push({ campaign_id: row[0],latitute: location.latitude,longtitude: location.longitude });
+          } catch (e) {
+            //console.log(e); // error in the above string (in this case, yes)!
+          }
+        });
+        if (coordinates.length === 0) {
+          coordinates.push({ campaign_id: 101,latitute: 40,longtitude: -70 });
+        }
+        console.log(getCoords()); // eslint-disable-line
+      });
+
+
     return `data:text/csv;charset=utf-8,${encodeURI(data)}`;
   }
 
   renderCSVSave() {
     const now = Math.floor(Date.now() / 1000);
-
     if (!this.props.saveCsv) {
       return null;
     }
