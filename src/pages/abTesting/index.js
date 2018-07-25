41eaf7500e46b7b27e8b5c70a77111b512df6e89
Jon Ambas <jon@jonambas.com>
2018-07-25 14:49:09 -0400

    FE-451 Add ab test details page wrapper

--- src/pages/abTesting/index.js
+++ src/pages/abTesting/index.js
@@ -1,5 +1,7 @@
 import ListPage from './ListPage';
+import DetailsPage from './DetailsPage';
 
 export default {
-  ListPage
+  ListPage,
+  DetailsPage
 };
--- src/reducers/abTesting.js
+++ src/reducers/abTesting.js
@@ -1,8 +1,10 @@
 const initialState = {
   list: [],
-  abTest: {},
   listLoading: true,
-  listError: null
+  listError: null,
+  detailsById: {},
+  detailsLoading: false,
+  detailsError: null
 };
 
 export default (state = initialState, action) => {
@@ -17,6 +19,26 @@ export default (state = initialState, action) => {
 
     case 'LIST_AB_TESTS_SUCCESS':
       return { ...state, list: action.payload, listLoading: false };
+
+    /* Details */
+    case 'GET_AB_TEST_PENDING':
+      return { ...state, detailsLoading: true, detailsError: null };
+
+    case 'GET_AB_TEST_FAIL':
+      return { ...state, detailsError: action.payload, detailsLoading: false };
+
+    case 'GET_AB_TEST_SUCCESS':
+      return {
+        ...state,
+        detailsLoading: false,
+        detailsById: {
+          ...state.detailsById,
+          [action.payload.id]: {
+            ...state.detailsById[action.payload.id],
+            [`version_${action.payload.version}`]: action.payload
+          }
+        }
+      };
 
     default:
       return state;
